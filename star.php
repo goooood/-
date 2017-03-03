9d1aefc42df55a0c9d1f1fa3ab5409ab916ddb3d
mayonghui <784611361@qq.com>
2017-03-03 16:23:36 +0800

    一些微小调整

--- star.php
+++ star.php
@@ -15,7 +15,7 @@ function asterisk_name($name) {
     } else if ($len == 4) { //复姓
         return mb_substr($name, 0, 2, 'utf8') . '**';
     } else { //
-        return $name;
+        return false;
     }
 }
 
@@ -29,7 +29,7 @@ function asterisk_user_name($name) {
 function asterisk_email($email) {
     $username_suffix = explode('@', $email);
     if (!isset($username_suffix[1])) {
-        return $email;
+        return false;
     }
     $len = strlen($username_suffix[0]);
     $sub_len = $len / 2;
@@ -47,24 +47,26 @@ function asterisk_id($id) {
     } else if ($len == 15) {
         return substr($id, 0, 6) . '******' . substr($id, -3);
     } else { //不正确的身份证号
-        return $id;
+        return false;
     }
 }
 
 //电话号码,目前仅仅支持11位手机号
 function asterisk_phone($phone) {
     if (empty($phone)) {
-        return $phone;
+        return false;
     }
     return substr($phone, 0, 3) . '****' . substr($phone, -4);
 }
 
 /*echo asterisk_name('上官飞扬'), "\n";
+echo asterisk_user_name('abcdefg'), "\n";
 echo asterisk_email('123456@qq.com'), "\n";
 echo asterisk_id('222301880101242'), "\n";
 echo asterisk_phone(18224535585), "\n";
 echo '------------',"\n";
 echo asterisk_name(''), "\n";
+echo asterisk_user_name(''), "\n";
 echo asterisk_email(''), "\n";
 echo asterisk_id('132456'), "\n";
 echo asterisk_phone(18224535585), "\n";*/
